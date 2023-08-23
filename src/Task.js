73fd8025525398a2a7df8911b2a3508dc7a96d3f
SeyyitBattal <seyyitbattalarvas3@gmail.com>
2023-08-23 10:09:33 +0300

    formatDistanceToNow is working

--- src/Task.js
+++ src/Task.js
@@ -4,16 +4,23 @@ import { differenceInDays } from "date-fns";
 
 const Task = ({ taskObj, onComplete }) => {
   const result = differenceInDays(new Date(taskObj.deadline), new Date());
+  const simdi = formatDistanceToNow(new Date(taskObj.deadline), {
+    addSuffix: true,
+  });
   return (
     <div className="task">
       <h3>{taskObj.title}</h3>
       <div className="deadline">
         son teslim:
-        <h1 className="bg-orange-800">
-          {result < 4 && <p>{result} gün sonra</p>}{" "}
-        </h1>
         <h1 className="bg-teal-500">
           {result > 4 && <p>{result} gün sonra</p>}
+          {simdi}
+        </h1>
+        <h1 className="bg-rose-500">
+          {result < 4 && result > 0 && <p>{result} gün sonra</p>}
+        </h1>
+        <h1 className="bg-rose-500">
+          {result < 0 && <p>{result} gün geçti</p>}
         </h1>
         <span>{taskObj.deadline}</span>
       </div>
