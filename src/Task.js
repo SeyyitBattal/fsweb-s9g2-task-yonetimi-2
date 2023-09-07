e48f4aeea517d874c901a919a6d2795a2f4783c4
SeyyitBattal <seyyitbattalarvas3@gmail.com>
2023-09-07 17:08:19 +0300

    All done

--- src/Task.js
+++ src/Task.js
@@ -4,6 +4,7 @@ import { differenceInDays } from "date-fns";
 
 const Task = ({ taskObj, onComplete }) => {
   const result = differenceInDays(new Date(taskObj.deadline), new Date());
+
   const simdi = formatDistanceToNow(new Date(taskObj.deadline), {
     addSuffix: true,
   });
@@ -12,16 +13,7 @@ const Task = ({ taskObj, onComplete }) => {
       <h3>{taskObj.title}</h3>
       <div className="deadline">
         son teslim:
-        <h1 className="bg-teal-500">
-          {result > 4 && <p>{result} gün sonra</p>}
-          {simdi}
-        </h1>
-        <h1 className="bg-rose-500">
-          {result < 4 && result > 0 && <p>{result} gün sonra</p>}
-        </h1>
-        <h1 className="bg-rose-500">
-          {result < 0 && <p>{result} gün geçti</p>}
-        </h1>
+        <h1 className="bg-teal-300">{simdi}</h1>
         <span>{taskObj.deadline}</span>
       </div>
       <p>{taskObj.description}</p>
